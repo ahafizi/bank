80e7c7cad741d4690af915623dd27c7d514ddfe5
Abdullo <a.hafizov@team.alif.tj>
2020-09-27 20:27:45 +0500

    first initialize

---
+++ pkg/types/types.go
@@ -0,0 +1,52 @@
+package types
+
+// представляет собой денежную сумму в минимальных единицах (центы, дирамы, копейки и т.д)
+type Money int64
+
+type Currency string
+
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+	EUR Currency = "EUR"
+)
+
+type PAN string
+
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
+
+type PaymentSource struct {
+	Type    string
+	Number  string
+	Balance Money
+}
+
+// Category представляет собой категорию, в которой был совершен платёж (авто, аптеки, рестораны и т.д)
+type Category string
+
+//// Status представляет с собой статус платежа.
+//type Status string
+//
+//// Переопределенные статусы платежей.
+//const (
+//	StatusOk Status = "OK"
+//	StatusFail Status = "FAIL"
+//	StatusInProgress Status = "INPROGRESS"
+//)
+
+// Payment представляет с собой информацию о платеже
+type Payment struct {
+	ID       int
+	Amount   Money
+	Category Category
+	//Status   Status
+}
